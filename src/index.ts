638e5d6e09516fd93f4539a92b9bc03cc17224cd
Ali Sabzevari <alisabzevari@gmail.com>
2019-12-31 21:36:05 +0100

    Add api docs and improve project structure

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import * as http from "http";
+
+import {
+  HttpRequest,
+  HttpResponse,
+  HttpBodyImpl,
+  HttpStatus,
+  toNodeRequestListener
+} from "http4ts";
+
+async function handler(req: HttpRequest): Promise<HttpResponse> {
+  await req.body.asString("UTF-8");
+  return {
+    body: HttpBodyImpl.fromString("This is some string"),
+    headers: {},
+    status: HttpStatus.OK
+  };
+}
+
+const server = http.createServer(toNodeRequestListener(handler));
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
