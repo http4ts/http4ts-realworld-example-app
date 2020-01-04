e384f29fec06e0c5e3efc3a70f0a3c225f6444db
Ali Sabzevari <alisabzevari@gmail.com>
2020-01-04 13:10:43 +0100

    Make Logger interface independent of pino

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,67 @@
 import * as pino from "pino";
 
-export function createLogger() {
+export interface LogFn {
+  (msg: string, ...args: any[]): void;
+  (obj: object, msg?: string, ...args: any[]): void;
+}
+
+export interface Logger {
+  /**
+   * Log at `'fatal'` level the given msg. If the first argument is an object, all its properties will be included in the JSON line.
+   * If more args follows `msg`, these will be used to format `msg` using `util.format`.
+   *
+   * @param obj: object to be serialized
+   * @param msg: the log message to write
+   * @param ...args: format string values when `msg` is a format string
+   */
+  fatal: LogFn;
+  /**
+   * Log at `'error'` level the given msg. If the first argument is an object, all its properties will be included in the JSON line.
+   * If more args follows `msg`, these will be used to format `msg` using `util.format`.
+   *
+   * @param obj: object to be serialized
+   * @param msg: the log message to write
+   * @param ...args: format string values when `msg` is a format string
+   */
+  error: LogFn;
+  /**
+   * Log at `'warn'` level the given msg. If the first argument is an object, all its properties will be included in the JSON line.
+   * If more args follows `msg`, these will be used to format `msg` using `util.format`.
+   *
+   * @param obj: object to be serialized
+   * @param msg: the log message to write
+   * @param ...args: format string values when `msg` is a format string
+   */
+  warn: LogFn;
+  /**
+   * Log at `'info'` level the given msg. If the first argument is an object, all its properties will be included in the JSON line.
+   * If more args follows `msg`, these will be used to format `msg` using `util.format`.
+   *
+   * @param obj: object to be serialized
+   * @param msg: the log message to write
+   * @param ...args: format string values when `msg` is a format string
+   */
+  info: LogFn;
+  /**
+   * Log at `'debug'` level the given msg. If the first argument is an object, all its properties will be included in the JSON line.
+   * If more args follows `msg`, these will be used to format `msg` using `util.format`.
+   *
+   * @param obj: object to be serialized
+   * @param msg: the log message to write
+   * @param ...args: format string values when `msg` is a format string
+   */
+  debug: LogFn;
+  /**
+   * Log at `'trace'` level the given msg. If the first argument is an object, all its properties will be included in the JSON line.
+   * If more args follows `msg`, these will be used to format `msg` using `util.format`.
+   *
+   * @param obj: object to be serialized
+   * @param msg: the log message to write
+   * @param ...args: format string values when `msg` is a format string
+   */
+  trace: LogFn;
+}
+
+export function createLogger(): Logger {
   return pino({ prettyPrint: true });
 }
